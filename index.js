a80abcfe5bf64afa7a6b96a6a1995c7f00adc831
camdyn <camdyn.bordelon@gmail.com>
2022-01-29 16:45:56 -0800

    added start script and changed port

--- index.js
+++ index.js
@@ -130,7 +130,8 @@ app.use((err, req, res, next) => {
     res.status(status).render("error/error.ejs", { err })
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("fuck you")
 })
 
